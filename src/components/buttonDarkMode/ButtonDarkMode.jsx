81c680e598bdf3df5d3bce558c4b0381a4e2b33e
Andrii Nyzhnyk <prostoandrew1994@gmail.com>
2024-04-13 11:49:53 +0300

    finished theme toggling

--- src/components/buttonDarkMode/ButtonDarkMode.jsx
+++ src/components/buttonDarkMode/ButtonDarkMode.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './style.css'
 import sun from './icons/sun.svg'
 import moon from './icons/moon.svg'
 
 function ButtonDarkMode() {
+
+    const [themeMode, setThemeMode] = useState('light')
+    const buttonRef = useRef(null)
+
+
+    const toggleDarkMode = () => {
+        setThemeMode((currentValue) => {
+            return currentValue === 'light' ? 'dark' : 'light'
+        })
+    }
+
+    useEffect(() => {
+        if (themeMode === 'dark') {
+            document.body.classList.add('dark')
+            buttonRef.current.classList.add('dark-mode-btn--active')
+        } else {
+            document.body.classList.remove('dark')
+            buttonRef.current.classList.remove('dark-mode-btn--active')
+        }
+    }, [themeMode])
+
+
     return (
-        <button className="dark-mode-btn">
+        <button ref={buttonRef} onClick={toggleDarkMode} className="dark-mode-btn">
             <img src={moon} alt="Light mode" className="dark-mode-btn__icon" />
             <img src={sun} alt="Dark mode" className="dark-mode-btn__icon" />
         </button>
